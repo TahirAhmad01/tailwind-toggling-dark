d5264a8088d0f88913cc8edfe4d6acf1e0f5b2ce
TahirAhmad01 <tahirahmad0234@gmail.com>
2022-11-08 19:45:27 +0600

    info added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        lightShadow: " -3px -3px 7px #ffffff, 3px 3px 5px #ceced1",
+        darkShadow: " 3px 3px 5px #1b1a1a, -3px -3px 5px #2b2b2b",
+        insetShadow: "inset -3px -3px 7px #ffffff, inset 3px 3px 5px #ceced1",
+        darkInsetShadow:
+          "inset -3px -3px 7px #393939, inset 3px 3px 5px #181818",
+      },
+    },
   },
   plugins: [],
 };
